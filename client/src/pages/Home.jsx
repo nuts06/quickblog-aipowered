a7572671eb757f4d119bbfb486bd98a845c970c9
Natasha <121818639+nuts06@users.noreply.github.com>
2025-06-19 18:38:12 +0530

    updated the home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import BlogList from '../components/BlogList'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <BlogList />
+      <Newsletter />
+      <Footer />
     </div>
   )
 }
